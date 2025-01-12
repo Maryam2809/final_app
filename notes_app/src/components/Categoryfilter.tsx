966654757187f8232d01bd094f443da91a831a38
Maryam2809 <ec24772@qmul.ac.uk>
2025-01-12 19:30:45 +0000

    splitting notes

---
+++ notes_app/src/components/Categoryfilter.tsx
@@ -0,0 +1,11 @@
+function Categoryfilter() {
+  return (
+    <div className="d-flex mb-3">
+      <button className="btn btn-link">All Notes</button>
+      <button className="btn btn-link">Recents</button>
+      <button className="btn btn-link">Favorites</button>
+    </div>
+  );
+}
+
+export default Categoryfilter;
