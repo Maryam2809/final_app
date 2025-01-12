0b24d3ce38345129953cd410128ca7f9893da671
Maryam2809 <ec24772@qmul.ac.uk>
2025-01-12 19:31:32 +0000

    laying out components

--- notes_app/src/App.tsx
+++ notes_app/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Categoryfilter from "./components/Categoryfilter";
+import Notecards from "./components/Notecards";
+import Createbutton from "./components/Createbutton";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container-fluid">
+      {" "}
+      <div className="row">
+        {""}
+        <div>
+          <Navbar />
+        </div>
+        <div className="col-md-3 bg-light" style={{ marginTop: "30px" }}>
+          <Sidebar />
+        </div>
+        <div className="col-md-9">
+          <div className="d-flex justify-content-between align-items-center mb-4">
+            <Header />
+            <Createbutton />
+          </div>
+          <Categoryfilter />
+          <div className="container">
+            <Notecards />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
+export default App;
