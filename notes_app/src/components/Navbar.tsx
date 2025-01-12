41da18dd04b30d71f10ffb7dff19882f1a49f8e9
Maryam2809 <ec24772@qmul.ac.uk>
2025-01-12 19:28:47 +0000

    navigation bar for search

---
+++ notes_app/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+function Navbar() {
+  return (
+    <nav className="col-md-3 navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#mynavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="mynavbar">
+          <ul className="navbar-nav me-auto"></ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="text"
+              placeholder="Search"
+            />
+            <button className="btn btn-primary" type="button">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
