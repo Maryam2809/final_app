f60e95ee740a3835317f927bddfdc5149992f57e
Maryam2809 <ec24772@qmul.ac.uk>
2025-01-12 19:32:06 +0000

    creating header title

---
+++ notes_app/src/components/Header.tsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <div className="d-flex justify-content-between align-items-center mt-4 mb-4">
+      <h2>Recent Notes</h2>
+    </div>
+  );
+}
+export default Header;
